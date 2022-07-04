f24014b8fdb7297677a182dcc52bf60658f84dc5
kmjenny <kjn3008@gmail.com>
2022-07-05 00:47:27 +0900

    수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
-from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    path('signup/', views.CreateUserView.as_view(), name='signup'),
-    path('signup/done', views.RegisteredView.as_view(), name="create_user_done"),
+    # path('signup/', CreateUserView.as_view(), name='signup'),
+    # path('signup/done/', RegisteredView.as_view(), name='create_user_done'),
+    path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('signup/', signup, name="signup"),
 ]
--- simbathon/urls.py
+++ simbathon/urls.py
@@ -19,6 +19,6 @@ from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', views.showmain),
 ]
